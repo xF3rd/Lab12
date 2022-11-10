de8fc1556d53a787f3dd115d9f85eb94ee20f70c
xF3rd <fernando.bordin.correa@gmail.com>
2022-11-10 20:46:43 -0300

    finalizai chamaaaaa 🔥🔥🔥🔥🔥🔥

--- Lab12.asm
+++ Lab12.asm
@@ -10,17 +10,23 @@
 
 main proc
         mov ax,@data
-        mov ds,ax
+        mov ds,ax               ;inicia o data
 
-        CALL pega_matriz
+        CALL pega_matriz        ;chama funçao de pegar matriz                
         
         mov ah,2
-        mov dl,10
+        mov dl,10               ;pula linha
         int 21h
 
-        CALL escreve_matriz
+        CALL escreve_matriz     ;chama funçao para escrever matriz
 
-        mov ah,4ch
+        mov ah,2
+        mov dl,10               ;pula linha
+        int 21h
+
+        call escreve_matriz_trans       ;chama funcao para escrever matriz transposta
+
+        mov ah,4ch              ;finaliza o programa
         int 21h
 main endp
 
@@ -28,7 +34,7 @@ main endp
 pega_matriz proc
 
         xor ax,ax
-        xor cx,cx
+        xor cx,cx           ;zera registradores
         xor bx,bx
 
         mov cl,16
@@ -41,10 +47,10 @@ pega_matriz proc
                 
                 
                 int 21h
-                mov matriz[bx],al
+                mov matriz[bx],al           ;armazena matriz
                 inc bx
                 dec cl
-                jmp volta1
+                jmp volta1                  ;volta    
 
             pula:
 
@@ -57,11 +63,11 @@ escreve_matriz proc
 
         xor ax,ax
         xor bx,bx
-        xor cx,cx
+        xor cx,cx           ;zera registradores
         xor dx,dx
 
         mov AH,2
-        mov dl,10
+        mov dl,10           ;pula linha    
         int 21h
 
         mov ah,02
@@ -74,32 +80,79 @@ escreve_matriz proc
             cmp bx,4
             je pula_linha
             cmp bx,8
-            je pula_linha
+            je pula_linha           ;verifa contador
             cmp bx,12
             je pula_linha
             jmp print
             
             pula_linha:
             mov ah,2
-            mov dl,10
+            mov dl,10               ;pula linha
             int 21h
             
             print:
-            mov dl,matriz[bx]
+            mov dl,matriz[bx]       ;printa matriz
             int 21h
             
             mov dl,' '
-            int 21h
+            int 21h                  ;printa espaco
 
             inc bx
-            jmp volta2
+            jmp volta2               ;volta
 
         pula2:
-            ret
+            ret                     ;volta pra main
 
         
 
 escreve_matriz endp
+
+escreve_matriz_trans proc
+
+        xor ax,ax
+        xor bx,bx
+        xor cx,cx               ;verifa contador
+        xor dx,dx
+        xor si,si
+
+        mov AH,2
+        mov dl,10               ;pula linha
+        int 21h
+
+
+        volta3:
+
+            cmp bx,4            ;verifa contador
+            je pula3        
+
+            mov dl,matriz[si][bx]           ;printa matriz
+            int 21h
+
+            mov dl,' '
+            int 21h                 ;printa espaco
+
+
+
+            add si,4
+            cmp si,16               ;printa espaco
+            je sai
+            jmp volta3
+
+            sai:
+                mov ah,2
+                mov dl,10           ;pula linha
+                int 21h
+
+                inc bx
+                xor si,si           ;zera registrador
+                jmp volta3          ;pula
+
+
+        pula3:
+            ret                     ;volta pra main
+
+escreve_matriz_trans endp
+
 end main
 
 
